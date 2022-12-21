2285eb5d10fc1e6550d7bf9789452be5a421c30d
Jared Siemen <siemenjm@gmail.com>
2022-12-20 19:25:14 -0500

    Create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Amazona
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>footer</footer>
+      </div>
+    </>
+  );
+}
